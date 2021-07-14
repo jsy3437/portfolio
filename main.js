a2d4c9f1a9a63571e001c573b2203cc47594b8e9
jsy3437 <gusdo3437@naver.com>
2021-07-14 20:11:28 +0900

    JS: work filtering

--- main.js
+++ main.js
@@ -50,6 +50,29 @@ arrowUp.addEventListener('click', () => {
 	scrollIntoView('#home');
 });
 
+// Projects
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const Projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (e) => {
+	const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+	if (filter == null) {
+		return;
+	}
+	projectContainer.classList.add('anim-out');
+	setTimeout(() => {
+		Projects.forEach((project) => {
+			if (filter === '*' || filter === project.dataset.type) {
+				project.classList.remove('invisible');
+			} else {
+				project.classList.add('invisible');
+			}
+		});
+
+		projectContainer.classList.remove('anim-out');
+	}, 300);
+});
+
 function scrollIntoView(selector) {
 	const scrollTo = document.querySelector(selector);
 	scrollTo.scrollIntoView({ behavior: 'smooth' });
